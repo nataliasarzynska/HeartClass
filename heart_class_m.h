9b2b297009eb87e7d363107d969f74464e15a3b4
MSzeligiewicz <maria.szeligiewicz@gmail.com>
2023-01-04 16:18:30 +0100

    finished classes - not working

--- heart_class_m.h
+++ heart_class_m.h
@@ -1,110 +1,46 @@
 #ifndef DADM_HEART_CLASS_H
 #define DADM_HEART_CLASS_H
 
-//--------------------------------------------------------------------------------
-/* Includes */
-/* MARYSIA TO ZMIENILA !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ateraz natalka*/
-
 #include <complex>
 #include <iostream>
 #include <valarray>
 #include <vector>
 #include <memory>
 #include <numeric>
-
-//#include "../Waves/waves.h"
-//#include "dbscan.h"
+struct WavesFeatures {
+    std::vector<int> QRSonset;
+    std::vector<int> QRSend;
+    std::vector<int> T;
+    std::vector<int> Tend;
+    std::vector<int> P;
+    std::vector<int> Ponset;
+    std::vector<int> Pend;
+};
 
 //--------------------------------------------------------------------------------------
 /* Public types */
 
-/*
- * @brief           Possible types of detected activation
- */
 enum type{
-    DIFF_DISEASE = 0,        //< Ventricular activation
-    VENTRICULAR,       //< Supraventricular activation
+    DIFF_DISEASE = 0,        
+    VENTRICULAR = 1,
+    SUPRAVENTRICULAR = 2,
+    ARTIFACT = 3,     
 };
 
-/*
- * @brief           Struct for storing index and type of classification
- */
 typedef struct activations{
-    float Ridx;             //< index of R peak associated with activation
-    enum type actType;      //< activation type, see enum type
+    float Ridx;             
+    enum type actType;     
 }activations_t;
 
-/*
- * @brief           Struct for storing index and type of classification
- */
-typedef struct clusters{
-    float Ridx;             //< index of R peak associated with activation
-    uint8_t ClusterID;      //< ID of classified cluster
-}clusters_t;
-
-/*
- * @brief           Main class of the module.
- * @note            After calling the constructor you can get pointers to vectors of
- *                  classifications with appropriate getters.
- */
 class heart_class {
 private:
-/*
- * @brief           Function for checking if there is AV dissociation in the current part of signal.
- * @note            AV dissociation means that the relation between P-waves and QRS complexes is completely lost.
- *                  P-wave fluctuates back and forth across QRS complexes.
- * @param[in]       waves - pointer to waves of signal
- * @param[in]       currR - current R peak diagnosed
- * @return          classification of activation.
- */
-    enum type CheckAVDissociation(std::vector<int> rPeaks, int currR);
-
-/*
- * @brief           Function for classifying activations.
- * @param[in]       rPeaks - pointer to vector of detected R peaks
- * @param[in]       waves - pointer to waves of signal
- * @param[in]       fs - sampling frequency
- */
-    void Classify_Type(std::shared_ptr<std::vector<int>> rPeaks, std::vector<int> QRSend, int fs);
-
-/*
- * @brief           Function for classifying activations.
- * @param[in]       rPeaks - pointer to vector of detected R peaks
- * @param[in]       waves - pointer to waves of signal
- * @param[in]       fs - sampling frequency
- */
-     void Classify_Cluster(std::shared_ptr<std::vector<int>> rPeaks, std::vector<int> QRSend, int fs);
-
-/*
- * @brief           Pointer to vector of activations.
- */
+    enum type CheckAVDissociation(std::shared_ptr<WavesFeatures> waves, int currR);
+    void Classify_Type(std::shared_ptr<std::vector<int>> rPeaks, std::shared_ptr<WavesFeatures> waves, int fs);
     std::vector<activations_t> activations;
 
-/*
- * @brief           Pointer to vector of clusters.
- */
-    std::vector<clusters_t> clusters;
-
 public:
-
-/*
- * @brief           Constructor. Calles processing functions.
- * @param[in]       rPeaks - pointer to vector of detected R peaks
- * @param[in]       waves - pointer to waves of signal
- * @param[in]       fs - sampling frequency
- */
-    heart_class(std::shared_ptr<std::vector<int>> rPeaks, std::vector<int> QRSend, int fs);
-
-/*
- * @brief           Getter function for activation types.
- * return           pointer to vector of activation types.
- */
+    heart_class(std::shared_ptr<std::vector<int>> rPeaks, std::shared_ptr<WavesFeatures> waves, int fs);
     std::shared_ptr<std::vector<activations_t>> heart_classGetActivations();
-
-/*
- * @brief           Getter function for activation types.
- * return           pointer to vector of clusters.
- */
-    std::shared_ptr<std::vector<clusters_t>> heart_classGetClusters();
 };
+
 #endif //DADM_HEART_CLASS_H
--- main_natalka.cpp
+++ main_natalka.cpp
@@ -13,9 +13,6 @@ const string QRSonset_path = "QRS-onset.txt";
 const string Ponset_path = "P-onset.txt";
 const string Pend_path = "P-end.txt";
 
-
-// ---------- data loader ----------
-
 vector<float> loadSignal() {
     cout << "[INFO] Loading signal..." << endl;
 
@@ -67,6 +64,7 @@ vector<int> loadQRSonset() {
 
     return QRSonset;
 }
+
 vector<int> loadP() {
     cout << "[INFO] Loading P onset..." << endl;
 
@@ -82,36 +80,30 @@ vector<int> loadP() {
 
 // ------------------ CONSTANTS -----------------------
 
-/* Threshold interval based on Brugada algorithm */
+// RS length threshold based on Brugada algorithm 
 #define R_S_INTERVAL            60
 
-/* Number of intervals used for AV dissociation analysis */
+// Number of intervals used for AV dissociation analysis 
 #define NUM_OF_INTERVALS        10
 
-/* Standard deviation threshold */
+// Standard deviation threshold (AV dissociation analysis)
 #define STD_TH                  10
 
-/* Threshold value for detecting correct QRSonset/end waves (in miliseconds) */
-#define QRS_INTERVAL            80
-
-int SamplesToMiliseconds(int value, int fs)
-{
+int SamplesToMiliseconds(int value, int fs) {
     return value*1000/fs;
 }
 
-enum type CheckAVDissociation(std::vector<int> rPeaks, std::vector<int> P, int currR) {
-
+enum type heart_class::CheckAVDissociation(std::shared_ptr<WavesFeatures> waves, int currR) {
     std::vector<int> intervals;
-    std::vector<int> QRSonset = {loadQRSonset()};
+    auto QRSonset = waves->QRSonset;
+    auto P = waves->P;
 
-    /* Measure last P-QRSonset intervals */
     auto itQRS = std::upper_bound(QRSonset.begin(), QRSonset.end(), currR);
     auto itP = std::upper_bound(P.begin(), P.end(), currR);
     int currInterval;
 
-    for(uint8_t i = 0; i < NUM_OF_INTERVALS; i++)
-    {
-        /* Check if reached end of signal (inna choroba by default) */
+    for(uint8_t i = 0; i < NUM_OF_INTERVALS; i++) {
+        // Sprawdzamy czy jest juz koniec sygnalu (DIFFERENT DISEASE by default)
         if(itQRS == QRSonset.end() || itP == P.end())
             return DIFF_DISEASE;
 
@@ -119,16 +111,14 @@ enum type CheckAVDissociation(std::vector<int> rPeaks, std::vector<int> P, int c
         intervals.push_back(currInterval);
 
         int currInterval_ms = SamplesToMiliseconds(currInterval, 360);
-
     }
 
-    /* Calculate standard deviation */
+    // Odchylenie standardowe
     int sum = std::accumulate(intervals.begin(), intervals.end(), 0.0);
     int mean = sum/intervals.size();
     int stdDev = 0;
     int temp;
-    for(const auto &el : intervals)
-    {
+    for(const auto &el : intervals) {
         temp = (el-mean)*(el-mean);
         stdDev += temp;
     }
@@ -143,15 +133,13 @@ enum type CheckAVDissociation(std::vector<int> rPeaks, std::vector<int> P, int c
         return VENTRICULAR;
 }
 
-void Classify_Type(std::vector<int> rPeaks, std::vector<int> P, std::vector<int> QRSend, std::vector<int> QRSonset, int fs) 
-    {
-    activations_t nextClassification;
+void heart_class::Classify_Type(std::shared_ptr<std::vector<int>> rPeaks, std::shared_ptr<WavesFeatures> waves, int fs)  {
+    activations_t nextClassification; // do tego zapisujemy diagnoze i wywolujemy to publicznie funkcja getActivations
 
-    auto S = QRSend;
-    auto Q = QRSonset; 
+    auto Q = waves->QRSonset;  // zalamki Q
+    auto S = waves->QRSend; // zalamki S
 
-    for(const auto &r : rPeaks)
-    {
+    for(const auto &r : *rPeaks) {
         // Szukamy zalamka Q
         auto it_q = std::lower_bound(Q.begin(), Q.end(), r); 
         it_q -= 1;
@@ -159,34 +147,34 @@ void Classify_Type(std::vector<int> rPeaks, std::vector<int> P, std::vector<int>
         // Szukamy zalamka S
         auto it_s = std::upper_bound(S.begin(), S.end(), r); 
         
-        if(it_s != S.end())
-        {
+        if(it_s != S.end()) {
             int intervalRS = SamplesToMiliseconds(*it_s - r , fs);
             int intervalQRS = SamplesToMiliseconds(*it_s - *it_q, fs);
 
             int QRS = *it_s - *it_q;
             int QRSms = SamplesToMiliseconds(QRS, fs);
 
-            if(QRSms < 180 && QRSms > 50) { // sprawdzenie czy to artefakt czy QRS
+            // sprawdzenie czy to artefakt czy QRS
+            if(QRSms < 180 && QRSms > 50) { 
                 if(QRSms >= 120) {
                     // szeroki - to znaczy ze cos jest nie tak!!!!!!
                     // VR albo inna choroba
-                    if(intervalRS < R_S_INTERVAL )
-                    {
+                    if(intervalRS < R_S_INTERVAL ) {
                         // Sprawdzamy czy jest dysocjacja
-                        nextClassification.actType = CheckAVDissociation(rPeaks, P, r);            
+                        nextClassification.actType = CheckAVDissociation(waves, r);            
 
                         if(nextClassification.actType == 1)
                         {
-                            cout << "VENTRICULAR\n" << endl; // wykrywamy dysocjacje -> TU MA BYC VENTRICULAR
+                            nextClassification.actType = VENTRICULAR;
+                            cout << "VENTRICULAR\n" << endl; // wykrywamy dysocjacje -> jest VENTRICULAR
                         }
                         else 
                         {
-                            cout << "DIFFERENT DISEASE\n" << endl; // nie ma dysocjacji -> chyba masz inna chorobe ziomus
+                            nextClassification.actType = DIFF_DISEASE;
+                            cout << "DIFFERENT DISEASE\n" << endl; // nie ma dysocjacji -> to musi byc inna choroba
                         }
                     }
-                    else
-                    {
+                    else {
                         // jest ventricular 
                         nextClassification.actType = VENTRICULAR;
                         cout << "If greater than 60 it favours VERTICULAR" << endl;
@@ -195,48 +183,30 @@ void Classify_Type(std::vector<int> rPeaks, std::vector<int> P, std::vector<int>
                 else {
                     // waski
                     // SVR
+                    nextClassification.actType = SUPRAVENTRICULAR;
                     cout << "SVR" << endl;
                 }
                 
             } else { // artefakt
                 cout << "Artefakt" << endl;
-                // return ARTEFAKT
-
+                nextClassification.actType = ARTIFACT;
             }
 
-            nextClassification.Ridx = r;
            
+            this->activations.push_back(nextClassification);
             cout << "----------------------------------- \n" << endl;
         }
     }
 }
 
+heart_class::heart_class(std::shared_ptr<std::vector<int>> rPeaks, std::shared_ptr<WavesFeatures> waves, int fs)
+{
+    Classify_Type(rPeaks, waves, fs);
+}
 
-int main(){
-
-    // load data
-    vector <float> filteredSignal {loadSignal()};
-    vector <int> rPeaks {loadRpeaks()};
-    vector<int> rPeaksLocs(rPeaks.begin(), rPeaks.end());
-    vector <int> QRSend {loadQRSend()};
-    vector <int> QRSonset {loadQRSonset()};
-    vector <int> P {loadP()};
-
-    // check if data was loaded correctly
-
-    cout << "count of samples in signal = " << filteredSignal.size() << endl;
-    cout << "count of R peaks = " << rPeaks.size() << endl;
-    cout << "count of QRSend = " << QRSend.size() << endl;
-
-
-    Classify_Type(rPeaks,P,  QRSend, QRSonset,  360); 
-
-  
-
-
-
-
-
-
+std::shared_ptr<std::vector<activations_t>> heart_class::heart_classGetActivations()
+{
+    cout<< "?????" << endl;
+    return std::make_shared<std::vector<activations_t>>(this->activations);
 }
 
